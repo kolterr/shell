1ffd5507dc2d13a3087b825f040241fdbb4e1293
kolter <wangy@cdeyun.com>
2018-03-01 23:14:36 +0800

    'docker'

---
+++ install-docker.sh
@@ -0,0 +1,18 @@
+
+
+### ubuntu
+
+apt-get update 
+
+#安装工具
+apt-get -y install apt-transport-https ca-certificates curl software-properties-commonapt-get -y install apt-transport-https ca-certificates curl software-properties-common
+
+#安装 GPG证书
+curl -fsSL http://mirrors.aliyun.com/docker-ce/linux/ubuntu/gpg |  apt-key add -
+
+#写入软件源信息
+add-apt-repository "deb [arch=amd64] http://mirrors.aliyun.com/docker-ce/linux/ubuntu $(lsb_release -cs) stable"
+
+apt-get -y update
+
+apt-get -y install docker.ce
