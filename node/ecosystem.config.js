56aee7bcc14607dee2a08597a2afa86532be38f6
kolter <wangy@cdeyun.com>
2018-03-06 22:14:04 +0800

    'update'

---
+++ node/ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+    {
+      name      : 'express-test',
+      script    : 'app.js',
+      env: {
+        COMMON_VARIABLE: 'true'
+      },
+      env_production : {
+        NODE_ENV: 'production'
+      }
+    }
+  ]
+};
